658fd6ac39f8974b8fe1380261e14e977dd4205e
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-06-26 14:11:22 -0500

    Update 3.1.2.cpp

--- 3 Strings/3.1.2.cpp
+++ 3 Strings/3.1.2.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main()
 {
     string phrase = "Giraffe Academy";
+    string phrasesub;
     
     // Check string length
     cout << phrase.length() << endl;
@@ -21,6 +22,10 @@ int main()
     cout << phrase.find("Aca") << endl;
     // cout << phrase.find("Aca", 0); // find starting from index 0
     
-    return 0;
+    // Substring method (start index, how many chars)
+    phrasesub = phrase.substr(8, 3);
     
+    cout << phrasesub;
+    
+    return 0;
 }
