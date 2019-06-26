6321a830bfc9dee2267c467c30b9acb6fa58e61a
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-06-26 15:57:18 -0500

    Create 6.1.1.cpp

---
+++ 6 Arrays/6.1.1.cpp
@@ -0,0 +1,21 @@
+// Arrays
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    int luckyNums[20] = {4, 8 ,15 ,16, 42};
+    
+    luckyNums[10] = 100;
+    
+    cout << luckyNums[10];
+    
+    
+    
+    
+    return 0;
+}
