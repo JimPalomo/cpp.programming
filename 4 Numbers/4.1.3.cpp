b6f4432b2a347b2ce238affee8706e6b97c255b7
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-06-26 15:38:30 -0500

    Create 4.1.3.cpp

---
+++ 4 Numbers/4.1.3.cpp
@@ -0,0 +1,33 @@
+# Numbers
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // power function
+    cout << pow(2,5) << endl;
+    
+    
+    // sqrt function
+    cout << sqrt(36) << endl;
+    
+    // round function 
+    cout << round(35.2) << endl;
+    
+    // round up function
+    cout << ceil(35.2) << endl;
+
+    // round down function
+    cout << floor(35.2) << endl;
+    
+    // prints out bigger number
+    cout << fmax(3,10) << endl;
+    
+    // prints out smaller number
+    cout << fmin(3,10) << endl;
+    
+    return 0;
+}
