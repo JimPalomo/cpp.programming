538c72873b824c768f49013e76592e9d8f7e816d
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-06-26 15:42:30 -0500

    Create 5.1.1.cpp

---
+++ 5 User Input/5.1.1.cpp
@@ -0,0 +1,19 @@
+// User Input: number
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    
+    cout << "Enter your age: ";
+    
+    cin >> age;
+    
+    cout << "You are " << age << " years old";
+    
+    return 0;
+}
