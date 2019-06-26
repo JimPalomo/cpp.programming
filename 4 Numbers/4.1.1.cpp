1c18ef68e9b3fcd5ac062148ec18af76812ade07
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-06-26 14:17:26 -0500

    Create 4.1.1.cpp

---
+++ 4 Numbers/4.1.1.cpp
@@ -0,0 +1,25 @@
+// Numbers: operations, printing, mod
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    // Printing numbers 
+    cout << -40.1232133 << endl;
+    
+    // Mathematical operations
+    cout << 7 / 5 << endl;      // numbers are rounded to assigned variables (int = whole numbers)
+    cout << 5 * 5 << endl;
+    cout << 1 + 2 << endl;
+    cout << 20 - 5 << endl;
+    cout << (20 + 1) * 10;
+    cout << "\n";
+    
+    // Mod operator
+    cout << 10 % 3;
+
+    return 0;
+}
