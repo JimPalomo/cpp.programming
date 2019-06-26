c95f667caa83419be5b041202c2631022ebb8773
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-06-26 18:38:06 -0500

    Create 15.1.1.cpp

---
+++ 15 Classes & Objects/15.1.1.cpp
@@ -0,0 +1,48 @@
+//  Pointers: dereferencing pointers 
+// instead of pointing to memory loc, point to description (storage)
+
+#include <iostream>
+
+using namespace std;
+
+class Book {
+    public:
+        string title;
+        string author;
+        int pages;
+};
+
+int main()
+{
+
+    // Creating book object book1
+    Book book1;
+    
+    book1.title = "Harry Potter";
+    book1.author = "JK Rowling";
+    book1.pages = 500;
+    
+    // Creating second book object
+    Book book2;
+    
+    book2.title = "Lord of the Rings";
+    book2.author = "Tolkein";
+    book2.pages = 700;
+    book2.title = "Hunger Games";
+    
+    // Printing out book information
+    cout << book1.title << endl;
+    cout << book1.author << endl;
+    cout << book1.pages << endl;
+    
+    cout << endl;
+    
+    cout << book2.title << endl;
+    cout << book2.author << endl;
+    cout << book2.pages << endl;
+
+    
+    
+    return 0;
+}
+
