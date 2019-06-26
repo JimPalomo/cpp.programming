b06e688055dbd4df84bf05f9fa2ede8db64ed709
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-06-26 16:16:12 -0500

    Create 9.1.1.cpp

---
+++ 9 If Statements/9.1.1.cpp
@@ -0,0 +1,24 @@
+// If statements
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    
+    bool isMale = true;
+    
+    if (isMale == true) {
+        cout << "You are a male";
+    
+    } else {
+        cout << "You are not a male";
+
+    }
+
+    
+    return 0;
+}
+
