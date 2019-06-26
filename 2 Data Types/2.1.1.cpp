e958f0183d3107008bea1fd0ceb7f5858994377d
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-06-26 13:54:09 -0500

    Create 2.1.1.cpp

---
+++ 2 Data Types/2.1.1.cpp
@@ -0,0 +1,28 @@
+// Data Types
+
+#include <iostream>
+
+// using this header for cout, cin, etc. 
+// Without header this must be applied: "std::cout <<"
+using namespace std;
+
+int main()
+{
+    
+    char grade = 'A';
+    
+    string phrase = "Giraffe Academy";
+    
+    int age = 50;
+
+    // stores more decimal points that float
+    double gpa = 2.3;
+    // float gpa = 2.3;
+    
+    bool isMale = true;
+    
+    // Testing data types
+    cout << grade << endl;
+
+    return 0;
+}
