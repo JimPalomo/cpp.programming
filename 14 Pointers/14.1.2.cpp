4e1d8b03476b0cd27965d30b56503e618501b818
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-06-26 18:21:41 -0500

    Create 14.1.2.cpp

---
+++ 14 Pointers/14.1.2.cpp
@@ -0,0 +1,31 @@
+//  Pointers
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+
+    int age = 19;
+    
+    // Creating a pointer variable
+    int *pAge = &age;
+    
+    double gpa = 2.7;
+    double *pGpa = &gpa;
+    
+    string name = "Mike";
+    string *pName = &name;
+    
+    // Printing pointer variables
+    cout << pAge << endl;
+    cout << pGpa << end;
+    cout << pName << endl;    
+
+    
+    
+    return 0;
+}
+
