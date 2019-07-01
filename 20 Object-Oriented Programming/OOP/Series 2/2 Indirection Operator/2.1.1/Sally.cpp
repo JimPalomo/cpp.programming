6c756ab39b530b10ddc4f7ed3a2e0002c475252b
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-07-01 18:11:39 -0500

    Create Sally.cpp

---
+++ 20 Object-Oriented Programming/OOP/Series 2/2 Indirection Operator/2.1.1/Sally.cpp
@@ -0,0 +1,14 @@
+// Create code here
+
+#include <iostream>
+#include "Sally.h"
+using namespace std;
+
+Sally::Sally() {
+
+}
+
+// "::" tells that the function belongs to Sally class
+void Sally::printCrap() {
+    cout << "did someone say steak?" << endl;
+}
