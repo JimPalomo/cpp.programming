74fb96765eb64c186243f0d6bff7f7a2564574b2
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-07-01 15:11:06 -0500

    Update and rename 20 Object-Oriented Programming/OOP/Series 2/1 Introduction/1.1.1.cpp to 20 Object-Oriented Programming/OOP/Series 2/1 Pointers/1.1.1.cpp

---
+++ 20 Object-Oriented Programming/OOP/Series 2/1 Pointers/1.1.1.cpp
@@ -0,0 +1,24 @@
+// Object-Oriented Programming: Pointers
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int fish = 5;
+
+    // Address operator
+    cout << &fish << endl;
+
+    // Creating a pointer
+    int *fishPointer; 
+
+    // Fishpointer = memory address of fish
+    fishPointer = &fish;
+
+    cout << fishPointer << endl;
+
+
+
+
+} 
