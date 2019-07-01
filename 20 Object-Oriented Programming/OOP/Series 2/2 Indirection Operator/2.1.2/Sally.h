6cd5fefd5e61488ed77fa806461b6ba69435d38d
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-07-01 18:38:33 -0500

    Create Sally.h

---
+++ 20 Object-Oriented Programming/OOP/Series 2/2 Indirection Operator/2.1.2/Sally.h
@@ -0,0 +1,16 @@
+// Create prototypes in header file (contains class definitions and functions)
+
+// If not defined, then define Sally_H
+#ifndef SALLY_H
+#define SALLY_H
+
+class Sally {
+    public: 
+        Sally();
+        void printCrap();
+    protected:
+    private:
+
+};
+
+#endif // SALLY_H
