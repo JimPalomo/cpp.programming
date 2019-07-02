b612a2f02344779f5b26977e810885f1e1eb967a
JimPalomo <JimPPalomo@gmail.com>
2019-07-01 20:57:03 -0500

    Update

--- Object-Oriented Programming/Series 2/3 Constructor/3.1.1.cpp
+++ Object-Oriented Programming/Series 2/3 Constructor/3.1.1.cpp
@@ -1,9 +1,34 @@
-
+// Object-Oriented Programming: Constructors
 
 #include <iostream>
+#include <string>
 using namespace std;
 
+class BuckysClass {
+    public:
+        // Constructors have the same name, sets values of members within an object
+        BuckysClass(string z) {
+            setName(z);
+        }
+
+        void setName(string x) {
+            name = x;
+        }
+        
+        string getName() {
+            return name;
+        }
+    private:
+        string name;
+
+};
+
 int main() {
+    BuckysClass bo("Lucky Bucky Roberts");
+    cout << bo.getName() << endl;
+
+    BuckysClass bo2("Sally McSalad");
+    cout << bo2.getName() << endl;
 
     return 0;
 }
