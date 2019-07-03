80fdb7f87d092be2ba52a346bba064f073695c9c
JimPalomo <JimPPalomo@gmail.com>
2019-07-03 13:54:33 -0500

    Update

--- Object-Oriented Programming/Series 2/10 Member Initialization list/10.1.1/Sally.cpp
+++ Object-Oriented Programming/Series 2/10 Member Initialization list/10.1.1/Sally.cpp
@@ -5,7 +5,9 @@ using namespace std;
 // Member initialization list in a constructor
 // Whenever there is a constant variable in the class, we use member initialization list
 Sally::Sally(int a, int b) 
-: regVar(a), constVar(b) {}
+: regVar(a), constVar(b) {
+    cout << "Hello World" << endl;
+}
 
 void Sally::print() {
     cout << "regular var is: " << regVar << " const variable is: " << constVar << endl;
