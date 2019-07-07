44cb76431895a8b0347d4aa952c44d14222f9a13
JimPalomo <43157036+JimPalomo@users.noreply.github.com>
2019-07-07 15:09:12 -0500

    Create 14.1.4.cpp

---
+++ Basics/14 Pointers/14.1.4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+
+    // Declaring a pointer to point to address of a
+    int* p = &a;
+    
+    // Referencing to the address of a
+    cout << p << endl; 
+
+    // Dereferencing int a -- pointing to the value of a
+    cout << *p << endl;
+}
