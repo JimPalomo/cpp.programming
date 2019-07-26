0827f28cf1339e0bb6608314e20983499b2acdf5
jimpalomo <jimppalomo@gmail.com>
2019-07-25 19:57:57 -0500

    Update

--- Data Structures/recursion.cpp
+++ Data Structures/recursion.cpp
@@ -2,7 +2,39 @@
 
 using namespace std;
 
+void counter(int);
+
+int factorial(int);
+
+
 int main() {
+    int num;
+    cout << "Type in an integer: ";
+    cin >> num;
+
+    counter(num);
+
+    cout << factorial(num) << endl;
 
     return 0;
+}
+
+
+
+void counter(int count) {
+    
+    // Base case
+    if (count >= 1) {
+        // counter(count - 1);     // Counts from 1 to 7 (reverse) due to recursive rules (repeat # of iterations after recursion)
+        cout << "Number: " << count << endl;
+        counter(count - 1);        // Normal recursion 7 to 1
+    }
+}
+
+int factorial(int count) {
+    if (count == 0 || count == 1) {
+        return 1;
+    } else if (count > 1) {
+        return (count * factorial(count - 1));
+    }
 }
